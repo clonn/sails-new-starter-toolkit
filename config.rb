c967a1c3d8094f2c379ccdd218adaba7ab828a20
clonn <clonncd@gmail.com>
2015-02-12 00:56:34 +0800

    update config2

--- config.rb
+++ config.rb
@@ -1,5 +1,6 @@
+require 'compass/import-once/activate'
+require 'font-awesome-sass'
 require 'bootstrap-sass'
-require 'compass'
 images_dir = "assets/styles/images/"
 sass_dir = "assets/styles/"
 css_dir = ".tmp/public/styles/"
